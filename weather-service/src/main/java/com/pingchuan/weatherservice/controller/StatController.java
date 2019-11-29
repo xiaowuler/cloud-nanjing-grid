88b8dff769fd4fd1dd9d5d82a970dd85559ee91c
xiaowuler <xiaowuler@163.com>
2019-11-29 09:57:24 +0800

    第一次提交

--- weather-service/src/main/java/com/pingchuan/weatherservice/controller/StatController.java
+++ weather-service/src/main/java/com/pingchuan/weatherservice/controller/StatController.java
@@ -1,11 +1,13 @@
 package com.pingchuan.weatherservice.controller;
 
+import com.pingchuan.dto.stat.RegionStat;
 import com.pingchuan.weatherservice.service.StatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("stat")
@@ -15,8 +17,8 @@ public class StatController {
     private StatService statService;
 
     @RequestMapping("/findAreaCallByTimeRange")
-    public void findAreaCallByTimeRange(String startTime, String endTime){
-        statService.findAreaCallByTimeRange(startTime, endTime);
+    public List<RegionStat> findAreaCallByTimeRange(String startTime, String endTime){
+        return statService.findAreaCallByTimeRange(startTime, endTime);
     }
 
 }
--- weather-service/src/main/java/com/pingchuan/weatherservice/service/StatService.java
+++ weather-service/src/main/java/com/pingchuan/weatherservice/service/StatService.java
@@ -1,13 +1,16 @@
 package com.pingchuan.weatherservice.service;
 
+import com.pingchuan.dto.stat.RegionStat;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @FeignClient(name="provider-mysql")
 public interface StatService {
 
     @RequestMapping(value = "stat/findAreaCallByTimeRange", method = RequestMethod.POST)
-    void findAreaCallByTimeRange(@RequestParam String startTime, @RequestParam String endTime);
+    List<RegionStat> findAreaCallByTimeRange(@RequestParam String startTime, @RequestParam String endTime);
 }
