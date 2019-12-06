b6ecbb6aa838f4aff7b0f38ed7c88cd9115bf501
Administrator <xiaowuler@163.com>
2019-12-06 20:33:54 +0800

    19/12/06 修改接口文档

--- weather-service/src/main/resources/static/js/word/word.js
+++ weather-service/src/main/resources/static/js/word/word.js
@@ -1,10 +1,11 @@
 var App = function () {
-    this.timer = null;
+    this.CurrentTime = new CurrentTime(this);
 
     this.Startup = function () {
         this.ReLayout();
-        this.InitCurrentTime();
-        this.SetTimeOut();
+        this.CurrentTime.InitTime();
+        $('.word-aside ul li').on('click', this.OnAsideSelect.bind(this));
+
         window.onresize = this.ReLayout.bind(this);
     };
 
@@ -13,32 +14,17 @@ var App = function () {
         var height = $(window).height();
         var content = $('.content');
         content.width((width - 20)).height(height - 94);
-        $('.word-width').height(content.height() - 57);
+        $('.word-width').height(content.height() - 39);
     };
 
-    this.InitCurrentTime = function () {
-        var date = new Date();
-        var year = date.getFullYear();
-        var month = date.getMonth() + 1;
-        var day = date.getDate();
-        var hour = date.getHours();
-        var minute = date.getMinutes();
-        var second = date.getSeconds();
-        var weekend = new Array("星期日","星期一","星期二","星期三","星期四","星期五","星期六");
-        var currentDate = year + '-' + this.GetTimeFormatter(month) + '-' + this.GetTimeFormatter(day) + ' ' + this.GetTimeFormatter(hour) + ':' + this.GetTimeFormatter(minute) + ':' + this.GetTimeFormatter(second) + ' ' + weekend[date.getDay()];
+    this.OnAsideSelect = function (event) {
+        $('.word-aside ul li').removeClass("current");
+        $(event.target).addClass("current");
+        var path = $(event.target).attr('path');
 
-        $('#current-date').text(currentDate);
+        $('.iframe').attr('src', 'develop/{0}.html'.format(path));
+        $('#title').text($(event.target).text());
     };
-
-    this.SetTimeOut = function () {
-        this.timer = setInterval(this.InitCurrentTime.bind(this), 1000)
-    };
-
-    this.GetTimeFormatter = function (time) {
-        var format = time < 10 ? '0' + time : time;
-        return format;
-    };
-
 };
 
 $(document).ready(function () {
