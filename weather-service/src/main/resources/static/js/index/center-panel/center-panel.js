19f0ea23f819edbe125c54a671de601f73dc55cc
xiaowuler <xiaowuler@163.com>
2019-12-06 16:20:03 +0800

    2019/12/06 接口文档页面添加

--- weather-service/src/main/resources/static/js/index/center-panel/center-panel.js
+++ weather-service/src/main/resources/static/js/index/center-panel/center-panel.js
@@ -55,19 +55,21 @@ var CenterPanel = function () {
     }
 
     this.setUpdateTimes = function (data, i) {
+        var test = ['20191011', '20191012', '20191013', '20191011', '20191012', '20191013']
         var updateDateStr = "";
-        $(data).each(function(index, item){
-            var moment = this.timeUtils.parseStr(item.updateTime, 'YYYY-MM-DD HH:mm:ss');
-            var updateTime = moment.format('YYMMDDHH');
+        $(test).each(function(index, item){
+            //var moment = this.timeUtils.parseStr(item.updateTime, 'YYYY-MM-DD HH:mm:ss');
+            //var updateTime = moment.format('YYMMDDHH');
             if (index == i){
-                updateDateStr += '<li class="active"><span>'+ updateTime +'</span></li>';
+                updateDateStr += '<li class="active"><span>'+ item +'</span></li>';
             }else {
-                updateDateStr += '<li><span>'+ updateTime +'</span></li>';
+                updateDateStr += '<li><span>'+ item +'</span></li>';
             }
         }.bind(this));
         $('.update-time').html(updateDateStr);
 
-        this.setStartTime(data[i].startTimes, data[i].startTimes.length - 1, i);
+        //this.setStartTime(data[i].startTimes, data[i].startTimes.length - 1, i);
+        this.setStartTime(data[i].startTimes, data[i].startTimes.length - 1, 5);
     }
 
     this.setStartTime = function(startTimes, i, updateIndex){
@@ -101,6 +103,7 @@ var CenterPanel = function () {
         this.NewspaperTimeLine.bindClickEvent(startIndex, updateIndex, i);
         this.UpdateTimeLine.bindClickEvent(startIndex, updateIndex, i);
         this.ForecastTimeLine.bindClickEvent(startIndex, updateIndex, i);
+
         this.loadMap();
     }
 
